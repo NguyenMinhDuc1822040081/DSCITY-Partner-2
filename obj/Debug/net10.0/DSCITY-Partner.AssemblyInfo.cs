d43107eb4394993bd73e71798e324967b90684fd
NguyenMinhDuc1822040081 <1822040081@dntu.edu.vn>
2026-06-17 18:05:06 +0700

    test 1

--- obj/Debug/net10.0/DSCITY-Partner.AssemblyInfo.cs
+++ obj/Debug/net10.0/DSCITY-Partner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSCITY-Partner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0d1a9105960678746463c007845940741c178b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSCITY-Partner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSCITY-Partner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
